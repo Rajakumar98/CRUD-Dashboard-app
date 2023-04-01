afccc230bdd80ce39f3e0dab12023f419939b0fa
Rajakumar98 <rajakumar1998mech@gmail.com>
2023-04-01 16:12:13 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Adduser } from './Components/Adduser';
+import EditUsers from './Components/EditUser';
+import { Nopage } from './Components/NoPage';
+import Usercomponent from './Components/usercomponent';
+import { Userdetails } from './Components/Userdetails';
+import { data } from './Data/data';
+
+
 
 function App() {
+  const [user, setUser] =useState(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <Switch>
+
+      <Route exact path="/">
+      <Usercomponent
+      user={user}
+      setUser={setUser}
+      />
+      </Route>
+
+     <Route path="/add/user">
+        <Adduser
+        user={user}
+        setUser={setUser}
+        />
+     </Route>
+
+     <Route path="/edit/:ID">
+      <EditUsers
+      user={user}
+      setUser={setUser}
+      />
+
+     </Route>
+
+     <Route path="/user/:id"> 
+            <Userdetails
+            user={user}
+            />
+     </Route>
+
+     <Route path="/Students">
+        <Redirect path='/'></Redirect>
+
+     </Route>
+
+     <Route path="**">
+      <Nopage/>
+     </Route>
+
+     </Switch>
+
+    
     </div>
   );
 }
 
-export default App;
+export default  App;
+
+
+
